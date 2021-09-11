d7d2c691bd75bb4331672a67ddc6cb74216b9f08
Hicka <hello@hicka.dev>
2021-09-11 19:51:55 +0200

    initial: add frontend base

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './assets/logo.png';
+import './style/App.css';
+function App() {
+	return (
+		<div className='App'>
+			<img src={logo} alt='Trouvez les clés!' style={{width: '40%'}} />
+			<p>new random project haha</p>
+		</div >
+	);
+}
+
+export default App;
